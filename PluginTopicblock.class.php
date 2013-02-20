719a282567df9075f6e5d73f6d2cf01d890c0ad1
al.germis <al.germis@gmail.com>
2013-02-20 15:55:35 +0200

    Initial

---
+++ PluginTopicblock.class.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!class_exists('Plugin')) {
+	die('Hammmmmmmmmmertimmme!');
+}
+
+class PluginTopicblock extends Plugin {
+
+	public function Activate() {
+		return true;
+	}
+
+	public function  Deactivate() {
+		return true;
+	}
+
+	public function Init(){
+	}
+
+}
+?>
